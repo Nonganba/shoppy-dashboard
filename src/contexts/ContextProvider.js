5e0dcc1a54e0762fbdde55c4a48e3781279d5588
Nonganba <nanganba.th1109@gmail.com>
2022-12-12 16:32:37 +0530

    add Navbar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,12 +11,22 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
 
   return (
     <StateContext.Provider
       value={{
         activeMenu: activeMenu,
-        setActiveMenu: setActiveMenu
+        setActiveMenu: setActiveMenu,
+        isClicked: isClicked,
+        handleClick: handleClick,
+        screenSize: screenSize,
+        setScreenSize: setScreenSize
       }}
     >
       {children}
